f5463de16f9c44d128a0df37c2d098860841bcf4
oliebueno <ollibueno08@gmail.com>
2024-10-19 17:08:14 -0400

    Agregada una textura

--- src/main.js
+++ src/main.js
@@ -1,28 +1,33 @@
 import * as THREE from 'three';
 import createPlane from './plane';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 // Crear la escena
 const scene = new THREE.Scene();
-
+scene.background = new THREE.Color(0x87CEEB);
 // Crear la camara
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 // Crear el render
 const canvas = document.getElementById('myCanvas')
 const renderer = new THREE.WebGLRenderer({canvas: canvas});
 renderer.setSize(window.innerWidth, window.innerHeight);
 
+
 // Añadir el plano
 const plane = createPlane();
 scene.add(plane);
 
 // Posición de la camara
-camera.position.set(0, 10, 30);
+camera.position.set(0, 5, 200);
 
+// Configurar los controles de órbita
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.update();
 
 function animate() {
     requestAnimationFrame(animate);
-
+	controls.update();
     renderer.render(scene, camera);
 }
 animate()
--- src/plane.js
+++ src/plane.js
@@ -2,12 +2,12 @@ import * as THREE from 'three';
 
 // Crear un plano
 const createPlane = () => {
-	const geometry = new THREE.PlaneGeometry(10, 10, 5, 5);
-	const texture_1 = new THREE.TextureLoader().load('texture/Grass.png')
+	const geometry = new THREE.PlaneGeometry(100, 1000, 10, 10);
+	const texture_1 = new THREE.TextureLoader().load('src/texture/Grass.png')
 	texture_1.wrapS = THREE.RepeatWrapping;
 	texture_1.wrapT = THREE.RepeatWrapping;
-	texture_1.repeat.set(5, 5);
-    const material = new THREE.MeshBasicMaterial({ color: 0XFF00FF});
+	texture_1.repeat.set(10, 10);
+    const material = new THREE.MeshBasicMaterial({ map: texture_1, side: THREE.DoubleSide});
     const plane = new THREE.Mesh(geometry, material);
 	plane.rotation.x = -Math.PI / 2;
 
